61f72af78bea0efe5ffa5f5ebc1941b775f54b0e
Techworker <benjaminansbach@gmail.com>
2018-07-20 21:34:28 +0200

    changed titles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,19 +5,19 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Yearly</div>
+                    <div class="card-header">Yearly operations</div>
                     <canvas id="types_yearly" width="400" height="200"></canvas>
                 </div>
                 <div class="card">
-                    <div class="card-header">Monthly</div>
+                    <div class="card-header">Monthly operations</div>
                     <canvas id="types_monthly" width="400" height="200"></canvas>
                 </div>
                 <div class="card">
-                    <div class="card-header">Weekly</div>
+                    <div class="card-header">Weekly operations</div>
                     <canvas id="types_weekly" width="400" height="200"></canvas>
                 </div>
                 <div class="card">
-                    <div class="card-header">Daily</div>
+                    <div class="card-header">Daily operations</div>
                     <canvas id="types_daily" width="400" height="200"></canvas>
                 </div>
             </div>
